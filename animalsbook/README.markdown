39f5a3e08f61dcd9d19a9fddea06915f2eed0a05
Raphaela Ferraz <86068799+raphaelaferraz@users.noreply.github.com>
2021-08-10 16:25:25 -0300

    Arquivos do site

---
+++ animalsbook/README.markdown
@@ -0,0 +1,5 @@
+# Animalsbook
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/raphaelaferraz/pen/gOWZNGQ](https://codepen.io/raphaelaferraz/pen/gOWZNGQ).
+
+
---
+++ animalsbook/dist/script.js
@@ -0,0 +1,23 @@
+let button = document.getElementById("gostei");
+
+button.addEventListener("click", () => {
+  button.textContent = "Gostei 💙 ";
+});
+
+let button2 = document.getElementById("gostei2");
+
+button2.addEventListener("click", () => {
+  button2.textContent = "Gostei 💙 ";
+});
+
+let button3 = document.getElementById("gostei3");
+
+button3.addEventListener("click", () => {
+  button3.textContent = "Gostei 💙 ";
+});
+
+let button4 = document.getElementById("gostei4");
+
+button4.addEventListener("click", () => {
+  button4.textContent = "Gostei 💙 ";
+});
